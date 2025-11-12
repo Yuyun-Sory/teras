a1e1de551e3e7e4c84c28b4aca2b51679d8bd5a4
thessa midha <tessamidha@gmail.com>
2025-11-12 18:53:09 +0700

    sinkronisasi perubahan setelah rebase dan stash

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
+// Halaman utama
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.home'); // lowercase semua
+});
+
+// Halaman menu
+Route::get('/menu', function () {
+    return view('layouts.menu');
+})->name('menu');
+
+// Pencarian menu
+Route::get('/menu/search', function (Request $request) {
+    $query = $request->input('query');
+    return view('layouts.menu', ['query' => $query]);
+})->name('menu.search');
+
+// Halaman cara pesan
+Route::get('/cara-pesan', function () {
+    return view('layouts.cara-pesan');
+});
+
+// Halaman tentang
+Route::get('/tentang', function () {
+    return view('layouts.tentang');
 });
